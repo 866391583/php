cd28fe5719e033c14addf6d86cbfd9a4ccd6ff25
username <username@email.com>
2015-10-24 22:48:28 +0800

    abc

--- subfolder/test.rb
+++ subfolder/test.rb
@@ -1,13 +1,22 @@
+$LOAD_PATH << "."
+require "trig"
 puts "hello world"
 BEGIN{
     puts "begin"
 }
 def testBlock
     puts "testBlock"
+    include Trig
+    puts Trig::PI
+    puts Trig.sin(Trig::PI/6)
     yield
 end
 testBlock{
     puts "in block"
+    include Exp
+    puts Exp::E
+    puts Exp.abs(-2*Exp::E)
+
 }
 END{
     puts "end"
---
+++ subfolder/trig.rb
@@ -0,0 +1,12 @@
+module Trig
+    PI=3.141592653
+    def sin(n)
+        return Math.sin(n)
+    end
+end
+module Exp
+    E=2.71828
+    def abs(n)
+        return n.abs
+    end
+end
