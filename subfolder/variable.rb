f75662594b622cf36b95043c091b2bcb72b5c0d3
username <username@email.com>
2015-10-25 13:39:35 +0800

    abc

--- subfolder/variable.rb
+++ subfolder/variable.rb
@@ -1,9 +1,17 @@
 def fun
     $a=20
 end
+def upcase!(str)
+    str.upcase!
+end
 fun
 puts "#{$a}"
 $Abc="hello"
 $Abc=11
 puts $Abc.class
 puts 11/2
+@@A=30
+# puts defined(@@A)
+a="hello"
+upcase! a
+puts a
